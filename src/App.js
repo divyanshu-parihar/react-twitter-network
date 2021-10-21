1a3197807a67e4a25075a631562a1f9e1ed5e27d
Divyanshu Parihar <divyanshu1447@gmail.com>
2021-10-21 10:23:29 +0530

    basic dictionary word visualization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+import Graph from './components/graph/Graph';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  // useState
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-w-full border-box min-h-screen mx-auto w- text-center bg-gray-800">
+      <div className='container text-white'>A cool Project</div>
+      {/* <div className="container p- w-2 h-4/5 z-50 bg-gray-600">
+        <input className="ml-6 rounded text-white bg-gray-600 outline-none px-2"
+             type="text"
+             placeholder="Search ...."></input>
+      </div> */}
+      <Graph/>
     </div>
   );
 }
